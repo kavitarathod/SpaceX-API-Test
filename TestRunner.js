44aaadec34465870826e3b0538193652134b8d44
Kavitha <kavitarathod27@gmail.com>
2021-06-10 20:41:44 +0530

    first commit

--- TestRunner.js
+++ TestRunner.js
@@ -1,15 +1,15 @@
-// describe('Test Suite 1 : Get the Latest launch' , function(){
-//     require('./Tests/getLatestLaunch.js')
-// });
+describe('Test Suite 1: Get the Latest launch' , function(){
+    require('./Tests/getLatestLaunch.js')
+});
 
-// describe('Test Suite 1 : Create a  launch' , function(){
-//     require('./Tests/createLaunch.js')
-// });
+describe('Test Suite 2 : Create a  launch' , function(){
+    require('./Tests/createLaunch.js')
+});
 
-// describe('Test Suite 1 : Create a  launch' , function(){
-//     require('./Tests/deleteLaunch.js')
-// });
+describe('Test Suite 3 : Delete a  launch' , function(){
+    require('./Tests/deleteLaunch.js')
+});
 
-describe('Test Suite 1 : Create a  launch' , function(){
+describe('Test Suite 4 : Get One launch' , function(){
     require('./Tests/getOneLaunch.js')
 });
